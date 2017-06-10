ead93b55596ac44c5adb3e2eab0e7419e5f21a3b
Florian Cäsar <florian.caesar@outlook.com>
2017-06-10 23:56:57 +0200

    Add exposed Permute operation to float32 interop

--- src/DiffSharp/Interop.Float32.fs
+++ src/DiffSharp/Interop.Float32.fs
@@ -363,6 +363,7 @@ and DNDArray(m : ADDND) =
     static member Sign(a : DNDArray) = DNDArray(ADDND.Sign(a.asADDND))
     static member Sum(a : DNDArray) = DNumber(ADDND.Sum(a.asADDND))
     static member Transpose(a : DNDArray) = DNDArray(ADDND.Transpose(a.asADDND))
+    static member Permute(a : DNDArray, dims : int[]) = DNDArray(ADDND.Permute(a.asADDND, dims))
     static member Diagonal(a : DNDArray) = DVector(ADDND.Diagonal(a.asADDND))
     static member Trace(a : DNDArray) = DNumber(ADDND.Trace(a.asADDND))
     static member Solve(a : DNDArray, b : DVector) = DVector(ADDND.Solve(a.asADDND, b.asADDV))
